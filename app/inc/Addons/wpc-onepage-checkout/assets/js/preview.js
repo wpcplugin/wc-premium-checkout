aa9400ae0ef1bd6662b8ea6a30354625d9f23033
Willian Santana <willian_santana@live.com>
2020-11-22 22:57:47 -0300

    Fix bugs

--- app/inc/Addons/wpc-onepage-checkout/assets/js/preview.js
+++ app/inc/Addons/wpc-onepage-checkout/assets/js/preview.js
@@ -3,21 +3,21 @@
 	/* Container Max Width*/
 	api( 'wpc_theme_onepage_checkout_container_max_width', function( value ) {
 		value.bind( function( maxwidth ) {
-			document.documentElement.style.setProperty( '--base-max-width', maxwidth + 'px' );
+			document.documentElement.style.setProperty( '--wpc-base-max-width', maxwidth + 'px' );
 		});
 	});
 
 	/* Container Spacing */
 	api( 'wpc_theme_onepage_checkout_container_spacing', function( value ) {
 		value.bind( function( spacing ) {
-			document.documentElement.style.setProperty( '--base-spacing', spacing + 'px' );;
+			document.documentElement.style.setProperty( '--wpc-base-spacing', spacing + 'px' );;
 		});
 	});
 
 	/* Place Order Button */
 	api( 'wpc_theme_onepage_checkout_order_button_color', function( value ) {
 		value.bind( function( color ) {
-			document.documentElement.style.setProperty( '--order-button-color', color );
+			document.documentElement.style.setProperty( '--wpc-order-button-color', color );
 
 		});
 	});
@@ -25,7 +25,7 @@
 	/* Title Label */
 	api( 'wpc_theme_onepage_checkout_content_primary_color', function( value ) {
 		value.bind( function( color ) {
-			document.documentElement.style.setProperty( '--primary-background-color', color );
+			document.documentElement.style.setProperty( '--wpc-primary-background-color', color );
 		});
 	});
 
